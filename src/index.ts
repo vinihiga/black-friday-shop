c7ae7c5427a712e90322b0c736c0604089b56426
Vinicius Higa <viniciushigadev@gmail.com>
2026-07-30 22:35:48 -0300

    feat: add simple hello world

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-const message: string = "TypeScript is ready!";
-console.log(message);
+import express, {
+  json,
+  type Express,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Express = express();
+
+const myLogger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[LOG] Receiving request from ${req.ip} to ${req.path}`);
+  next();
+};
+
+app.use(express.json());
+app.use(myLogger);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Hello World!",
+  });
+});
+
+app.listen(3000);
